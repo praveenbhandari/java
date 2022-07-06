a2c1758020cd319f670875a54bc1a4f26c51bdba
Praveen <cardinalgracias31@gmail.com>
2022-07-06 11:57:55 +0530

    array deletion at index

--- new/array_deletion.java
+++ new/array_deletion.java
@@ -1,19 +1,32 @@
 class array_deletion{
-	public static void main(String[] args) {
-		int daa[] ={1,2,3,4,5,6,7,8,9};
+	static int daa[] ={1,2,3,4,5,6,7,8,9};
+	static int cap=daa.length;
 
-
-		for(int i=0;i<daa.length;i++){
-			if(daa[i] == 5){
-				for(int j=i;j<daa.length-1;j++){
+	void remove(int val){
+		for(int i=0;i<cap;i++){
+			if(daa[i] == val){
+				System.out .println("Removing "+val+ " at index "+i);
+				cap--;
+				for(int j=i;j<cap;j++){
 					daa[j] = daa[j+1];
 				}
 			}
 		}
+	}
 
-		for(int i=0;i<daa.length;i++){
+	void print(){
+		for(int i=0;i<cap;i++){
 			System.out.println(daa[i]);
 		}
+	}
+
+	public static void main(String[] args) {
+	array_deletion a=new array_deletion();
+	a.print();
+	a.remove(4);
+	a.print();
+
+
 
 	}
 }
